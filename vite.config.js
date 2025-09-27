f6e14c9e758099ed61299f9bd52e0dd24eca74a5
Eduardo <eduardo@internationalshowtimes.com>
2025-09-27 01:02:12 -0300

    First commit

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/nr/", // use your repo name here
+  plugins: [react()],
+});
+
